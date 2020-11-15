6348b315164b234b48c40955ae58c66ea55fae83
MatheusEstanislau <matheus.estanislau7@gmail.com>
2020-11-15 08:08:18 -0300

    model appointments relacionada com usuários

--- backend/src/models/appointments/index.ts
+++ backend/src/models/appointments/index.ts
@@ -4,15 +4,23 @@ import {
   PrimaryGeneratedColumn,
   CreateDateColumn,
   UpdateDateColumn,
+  JoinColumn,
+  ManyToOne,
 } from 'typeorm'
+
+import User from '../user'
 
 @Entity('appointments')
 class Appointments {
   @PrimaryGeneratedColumn('uuid')
   id: string
 
-  @Column('varchar')
-  provider: string
+  @Column('uuid')
+  provider_id: string
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'provider_id' })
+  provider: User
 
   @Column('time with time zone')
   date: Date
